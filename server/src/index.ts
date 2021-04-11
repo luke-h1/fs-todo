a08081b6e364d6af421616a0872b5ea6d3a44c53
luke-h1 <luke.howsam@yahoo.com>
2021-04-11 12:58:13 +0100

    add auth + basic todo stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,8 +10,7 @@ import passport from "passport";
 import cors from "cors";
 import { Todo } from "./entities/Todo";
 import { isAuth } from "./middleware/isAuth";
-import { profile } from "node:console";
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 
 const main = async () => {
   await createConnection({
@@ -64,4 +63,81 @@ const main = async () => {
       }
     )
   );
+
+  app.get("/auth/github", passport.authenticate("github", { session: false }));
+
+  app.get(
+    "/auth/github/callback",
+    passport.authenticate("github", { session: false }),
+    function (req: any, res) {
+      res.redirect(`http://localhost:54321/auth/${req.user.accessToken}`);
+    }
+  );
+
+  app.get(`/todo`, isAuth, async (req, res) => {
+    const todos = await Todo.find({
+      where: { creatorId: req.userId },
+      order: { id: "DESC" },
+    });
+    res.send({ todos });
+  });
+
+  app.post("/todo", isAuth, async (req , res) => {
+    const todo = await Todo.create({
+      text: req.body.text,
+      creatorId: req.userId,
+    }).save();
+    res.send({ todo });
+  });
+
+  app.put("/todo", isAuth, async (req, res) => {
+    const todo = await Todo.findOne(req.body.id);
+    if (!todo) {
+      res.send({ todo: null });
+      return;
+    }
+    if (todo.creatorId !== req.userId) {
+      throw new Error("not authorized");
+    }
+    todo.completed = !todo.completed;
+    await todo.save();
+    res.send({ todo });
+  });
+
+  app.get("/me", async (req, res) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      res.send({ user: null });
+      return;
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      res.send({ user: null });
+      return;
+    }
+
+    let userId = "";
+    try {
+      const payload: any = verify(token, process.env.ACCESS_TOKEN_SECRET);
+      userId = payload.userId;
+    } catch (e) {
+      res.send({ user: null });
+      return;
+    }
+    if (!userId) {
+      res.send({ user: null });
+      return;
+    }
+    const user = await User.findOne(userId);
+    res.send({ user });
+  });
+
+  app.get("/", (_, res) => {
+    res.send("API is running");
+  });
+
+  app.listen(3000, () => {
+    console.log("listening on 3000");
+  });
 };
+main().catch((e) => console.error(e));
