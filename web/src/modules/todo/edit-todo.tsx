2e7d00ae6889dea5f077199c13b4ae64f4a2560d
luke-h1 <luke.howsam@yahoo.com>
2021-04-12 20:27:50 +0100

    get id from router

--- web/src/modules/todo/edit-todo.tsx
+++ web/src/modules/todo/edit-todo.tsx
@@ -1,13 +1,17 @@
+/* eslint-disable radix */
+import { useRouter } from 'next/router';
 import React from 'react';
 
-interface EditTogoPageProps {
+export const EditTogoPage: React.FC<{}> = () => {
+  const router = useRouter();
+  const intId = typeof router.query.id === 'string' ? parseInt(router.query.id) : -1;
 
-}
-
-export const EditTogoPage: React.FC<EditTogoPageProps> = () => {
   return (
     <>
-      <h1>id page</h1>
+      <h1 className="text-3xl">
+        todo id is:
+        {intId}
+      </h1>
     </>
   );
 };
